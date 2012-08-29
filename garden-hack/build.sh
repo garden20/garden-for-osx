b2713f4ff6e00ce304cfe51f39ea6cd160e7e105
Ryan Ramage <ryan.ramage@gmail.com>
2012-08-29 15:44:05 -0600

    Hack the nib to change the menu items.

--- garden-hack/build.sh
+++ garden-hack/build.sh
@@ -7,10 +7,10 @@ mkdir dist
 unzip "$1" -d dist
 mv "dist/CouchDB Server.app" dist/Garden.app
 cp assets/Info.plist dist/Garden.app/Contents/Info.plist
+cp assets/MainMenu2.nib dist/Garden.app/Contents/Resources/MainMenu.nib
 cp assets/CouchDb.icns dist/Garden.app/Contents/Resources/CouchDb.icns
 cp assets/CouchDb-Status-bw.png dist/Garden.app/Contents/Resources/CouchDb-Status-bw.png
 cp assets/CouchDb-Status.png dist/Garden.app/Contents/Resources/CouchDb-Status.png
-cp assets/ensure_full_commit.sh dist/Garden.app/Contents/Resources/ensure_full_commit.sh
 cp assets/local.ini dist/Garden.app/Contents/Resources/couchdbx-core/etc/couchdb/local.ini
 cp -r temp/www dist/Garden.app/Contents/Resources/couchdbx-core/share/couchdb/
 ditto -ck --rsrc --sequesterRsrc dist temp/garden_for_osx.zip
