b6175ef2bed5c94a8fd1859c99aad324f339195e
Ryan Ramage <ryan.ramage@gmail.com>
2012-08-29 13:56:49 -0600

    Add the icons

--- garden-hack/build.sh
+++ garden-hack/build.sh
@@ -7,6 +7,7 @@ mkdir dist
 unzip "$1" -d dist
 mv "dist/CouchDB Server.app" dist/Garden.app
 cp assets/Info.plist dist/Garden.app/Contents/Info.plist
+cp assets/CouchDb.icns dist/Garden.app/Contents/Resources/CouchDb.icns
 cp assets/CouchDb-Status-bw.png dist/Garden.app/Contents/Resources/CouchDb-Status-bw.png
 cp assets/CouchDb-Status.png dist/Garden.app/Contents/Resources/CouchDb-Status.png
 cp assets/ensure_full_commit.sh dist/Garden.app/Contents/Resources/ensure_full_commit.sh
